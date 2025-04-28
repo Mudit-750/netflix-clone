7a8b1967f610cf5f48e38c46e587f3aa19f642cc
Mudit-750 <agarwalmudit2404@gmail.com>
2025-04-28 11:04:26 +0530

    Updates

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,6 +1,6 @@
 import React, { useState,useEffect, useRef } from "react";
 import "./TitleCards.css";
-import cards_data from "../../assets/cards/Cards_data";
+
 import { Link } from "react-router-dom";
 
 const TitleCards = ({title,category,type}) => {
